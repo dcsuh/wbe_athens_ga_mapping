3d317c4048181d6dd98843540af113dbd701d625
Megan Lott <meganejlott@gmail.com>
2020-10-28 09:38:46 -0400

    add in code files

---
+++ mapping.R
@@ -0,0 +1,184 @@
+library(raster)
+library(mapview)
+library(tidyverse)
+library(dplyr)
+library(sf)
+library(rgdal)
+library(rgeos)
+library(cleangeo)
+
+###CEDAR CREEK###
+
+#We are going to going to dissolve all of the geometries in the CC file into a 
+#single geometry for future use. 
+#We'll do this with the "sf" functions, since these are preferred by most when 
+#working with GIS data in R.
+
+#Read in the data. You should see ~5,000 parcels with geometries and owner info. 
+#We really don't need that extra information, so we will remove in a bit
+cedar_creek = st_read("./CCWRF/CCWRF.shp")
+
+#Add an additional column, speficying the WRF, so that we can join by this common variable
+cedar_creek$wrf = "CC"
+
+#Take a look at the data
+cedar_creek %>%
+  st_set_geometry(NULL) %>%
+  glimpse()
+
+#All of the dimensions of the Cedar Creek data are = 2
+#dimensions = as.data.frame(st_dimension(cedar_creek))
+
+#Now, use the summarize functions to group by "wrf"
+cedar_creek_dissolved =
+  cedar_creek %>%
+  group_by(wrf) %>% 
+  summarise()
+
+#view to make sure this worked. You should see only a single polygon when you hover. This should load faster now.
+mapview(cedar_creek_dissolved)
+
+#Check the sf, and you should see only two variables: wrf, geometry
+#Let's write the sf to save for later
+
+st_write(obj = cedar_creek_dissolved, dsn = 'cc_dissolved', driver = 'ESRI Shapefile', layer = NULL)
+
+###MIDDLE OCONEE###
+
+#Since we're having trouble with the Middle Oconee shapefiles &polygons, let's use a different method.
+#This method is described [here](https://philmikejones.me/tutorials/2015-09-03-dissolve-polygons-in-r/).
+
+#Download Middle Oconee Shapefile, using OGR
+region = readOGR(dsn = "MOWRF", layer ="MOWRF")
+
+#Because there are warnings, use cleangeo functions
+region = clgeo_Clean(region)
+
+# Check the shapefile has loaded correctly:
+# plot(region)
+
+# We're going to dissolve all regions in to one, for the catchment area for Middle Oconee.
+# For this we'll create a lookup table and merge with the spatial data, based on OBJECTID
+
+region@data = select(region@data, c("OBJECTID"))
+lu = data.frame()
+lu = rbind(lu, region@data)
+lu$OBJECTID = as.character(lu$OBJECTID)
+#lu$NAME = as.character(lu$NAME)
+lu$wrf = NA
+lu$wrf = "MI"
+
+# Merge
+# We now need to merge the lookup table into our spatial object data frame.
+# We should end up with one row per zone to dissolve, each with a reference for the relevant larger geography.
+# I think the trick is to make sure the row names match exactly, and if you can match the polygon IDs as well with spChFIDs().
+region@data$OBJECTID = as.character(region@data$OBJECTID)
+region@data = full_join(region@data, lu, by = "OBJECTID")
+
+# Ensure shapefile row.names and polygon IDs are sensible
+row.names(region) = row.names(region@data)
+region = spChFIDs(region, row.names(region))
+
+# Dissolve
+# I use gUnaryUnion() (and indeed I think unionSpatialPolygons() in the maptools package uses this by default).
+region = gUnaryUnion(region, id = region@data$wrf)
+
+# If you want to just plot this using base plot you can stop there.
+# If you want to do anything with the data or plot using ggplot you need to recreate the data frame.
+
+# If you want to recreate an object with a data frame make sure row names match
+row.names(region) = as.character(1:length(region))
+
+# Extract the data you want (the larger geography)
+lu = unique(lu$wrf)
+lu = as.data.frame(lu)
+colnames(lu) = "wrf"  # your data will probably have more than 1 row!
+
+# And add the data back in
+region = SpatialPolygonsDataFrame(region, lu)
+
+# Check it's all worked
+plot(region)
+
+#Now, let's make this into an sf for future use.
+middle_oconee_dissolved = as(region, "sf")
+
+#Make sure it looks the same and is saved as one object
+mapview(middle_oconee_dissolved)
+
+#Let's be sure to write the sf object to retrieve later if we need
+st_write(obj = middle_oconee_dissolved, dsn = 'mi_dissolved', driver = 'ESRI Shapefile', layer = NULL)
+
+
+###NORTH OCONEE###
+
+#Since we're also having trouble with the North Oconee shapefiles & polygons, let's use the method.
+# described [here](https://philmikejones.me/tutorials/2015-09-03-dissolve-polygons-in-r/).
+
+#Download Middle Oconee Shapefile, using OGR
+region = readOGR(dsn = "NOWRF", layer ="NOWRF")
+
+#This step will take a few seconds. You should have a large spatial polygon as the output.
+
+#Because there are warnings, use cleangeo functions
+region = clgeo_Clean(region)
+
+#We still have some errors, but let's see if we can see the figure properly
+plot(region)
+#Again, this will take some time since this file is so large & has so many seperate geometries
+
+# We're going to dissolve all regions in to one, for the catchment area for North Oconee.
+# For this we'll create a lookup table and merge with the spatial data, based on OBJECTID
+
+region@data = select(region@data, c("OBJECTID"))
+lu = data.frame()
+lu = rbind(lu, region@data)
+lu$OBJECTID = as.character(lu$OBJECTID)
+#lu$NAME = as.character(lu$NAME)
+lu$wrf = NA
+lu$wrf = "NO"
+
+# Merge
+# We now need to merge the lookup table into our spatial object data frame.
+# We should end up with one row per zone to dissolve, each with a reference for the relevant larger geography.
+# I think the trick is to make sure the row names match exactly, and if you can match the polygon IDs as well with spChFIDs().
+region@data$OBJECTID = as.character(region@data$OBJECTID)
+region@data = full_join(region@data, lu, by = "OBJECTID")
+
+# Ensure shapefile row.names and polygon IDs are sensible
+row.names(region) = row.names(region@data)
+region = spChFIDs(region, row.names(region))
+
+# Dissolve
+# I use gUnaryUnion() (and indeed I think unionSpatialPolygons() in the maptools package uses this by default).
+region = gUnaryUnion(region, id = region@data$wrf)
+
+# If you want to just plot this using base plot you can stop there.
+# If you want to do anything with the data or plot using ggplot you need to recreate the data frame.
+
+# If you want to recreate an object with a data frame make sure row names match
+row.names(region) = as.character(1:length(region))
+
+# Extract the data you want (the larger geography)
+lu = unique(lu$wrf)
+lu = as.data.frame(lu)
+colnames(lu) = "wrf"  # your data will probably have more than 1 row!
+
+# And add the data back in
+region = SpatialPolygonsDataFrame(region, lu)
+
+# Check it's all worked
+plot(region)
+
+#Now, let's make this into an sf for future use.
+north_oconee_dissolved = as(region, "sf")
+
+#Make sure it looks the same and is saved as one object
+mapview(middle_oconee_dissolved) + mapview(north_oconee_dissolved, col.region = "orange") + mapview(cedar_creek_dissolved, col.region = "green")
+
+#Let's be sure to write the sf object to retrieve later if we need
+st_write(obj = north_oconee_dissolved, dsn = 'no_dissolved', driver = 'ESRI Shapefile', layer = NULL)
+
+wrf_catchment = rbind(cedar_creek_dissolved, middle_oconee_dissolved, north_oconee_dissolved)
+
+st_write(obj = wrf_catchment, dsn = 'wrf_catchment', driver = 'ESRI Shapefile', layer = NULL)
---
+++ virus_by_catchment_area.R
@@ -0,0 +1,107 @@
+library(mapview)
+library(tidyverse)
+library(dplyr)
+library(sf)
+library(plyr)
+library(RColorBrewer)
+library(heat.col)
+
+
+#load viral data and then clean. Here, we're summarizing the two targets into a single value, by averaging.
+n1_n2 = read.csv("./n1_n2_cleaned.csv")
+n1_n2 = mutate(n1_n2, log_total_copies= log10(mean_total_copies))
+n1_n2 = plyr::ddply(n1_n2, c("wrf", "date"), summarize, total_copies = mean(log_total_copies))
+n1_n2 = n1_n2 %>% select(wrf, date, target, log_total_copies)
+
+#now join the viral data to the catchment regions.
+wrf_catchment = st_read("./wrf_catchment/wrf_catchment.shp")
+n1_n2_catchment = left_join(wrf_catchment, n1_n2, by = "wrf")
+n1_n2_catchment = as(n1_n2_catchment, "sf")
+
+#Right now, we can only look at one date at a time, until we can animate/add slider/tabs.
+n1_n2_catchment = n1_n2_catchment %>% filter(date == "2020-10-20")
+
+#And map, using Leaflet's mapview.
+mapview(n1_n2_catchment, zcol = "total_copies", at = seq(10, 16, 1))
+
+
+###STOP HERE###
+
+#Animation scripts are below from Hampton Roads. Still trouble with this.
+
+# Animated SARS-CoV-2 total viral load for southeast VA during the first ~3 months of detection in the area
+
+# this code creates and animates a map of total SARS-CoV-2 viral load (concentration in wastewater * flow at sample time) normalized
+# by the population of WWTP's catchment.  
+# the map has 3 components;
+#                       1. a base map of the region - created using the maps package
+#                       2. the catchment boundaries - imported shapefiles from our company's GIS dept.
+#                       3. viral load data          - generated from our 9 WWTP facilities
+#
+# the animation is made using the gganimate package
+
+
+# dependent packages
+
+library(ggplot2)
+library(ggthemes)
+library(sf)
+library(stringr)
+library(maps)
+library(mapdata)
+library(wesanderson)
+library(dplyr)
+library(tidyr)
+library(ggpubr)
+library(RCurl)
+library(pals)
+#install.packages("gganimate")
+library(gganimate)
+#install.packages("tansformr")
+#library(transformr) 
+
+# get base map of region
+
+counties <-  map_data('county')
+ga_counties <-  subset(counties, region == 'georgia')
+
+
+ga = st_as_sf(map('county', plot=F, fill=T )) 
+ga = subset(ga, grepl('georgia', ga$ID))
+
+# custom color scale
+pal <- wes_palette("Zissou1", 100, type = "continuous")
+
+
+# **** Map w/ Animation *****
+
+ggplot()+ geom_sf(data = ga, color='grey30', fill='antiquewhite') + 
+  geom_sf(data=n1_n2_catchment, aes(fill = log_total_copies), alpha=.80)
+
+
+  scale_fill_gradientn(colors = pal)+
+  labs(title = "SARS-CoV-2 Viral Load by TP Catchment Population", fill = expression(paste(Log[10], ' ', 'Viral Load')), 
+       subtitle = 'Date: {frame_time}')+
+  facet_wrap(~date)+
+  theme_map()+
+  theme(plot.subtitle = element_text(size = 12),
+        legend.text = element_text(size = 10),
+        legend.title = element_text(size = 12),
+        strip.background = element_rect(fill="aliceblue", colour="black"),
+        panel.border = element_rect(colour = 'black', fill=NA),
+        legend.position = 'bottom',
+        plot.title = element_text(size = 16))+
+  guides(fill = guide_colorbar(barwidth = 12, barheight = 1))
+
+
+# using animate function you can specify the transition speed in frames per second (fps)
+
+SARS2_slower <- animate(SARS2, fps = 5)
+
+SARS2_slower
+
+
+# save to directory
+
+anim_save('SARS2_slower.gif')
+
